1b31495db3b02813ddd490835ca10d2ce93acff6
arielgoes <arieloesdecastro@gmail.com>
2022-06-02 17:39:37 -0300

    new topology

--- labs/lab03/topo2.py
+++ labs/lab03/topo2.py
@@ -20,24 +20,29 @@ class Linear(Topo):
         h2 = self.addHost('h2', ip='10.1.1.2', defaultRoute='via 10.1.1.254')
         h3 = self.addHost('h3', ip='10.1.1.3', defaultRoute='via 10.1.1.254')
         h4 = self.addHost('h4', ip='10.1.1.4', defaultRoute='via 10.1.1.254')
-        #nat1 = self.addNode('nat1', ip='10.1.1.3', cls=NAT, inNamespace=False)
+        h5 = self.addHost('h5', ip='10.1.1.5', defaultRoute='via 10.1.1.254')
 
         #declaracao dos switches openFlow
         s1 = self.addSwitch('s1')
         s2 = self.addSwitch('s2')
         s3 = self.addSwitch('s3')
         s4 = self.addSwitch('s4')
+        s5 = self.addSwitch('s5')
 
-       #criacao dos links entre hosts-switches e switches-switches
+        #criacao dos links entre hosts-switches e switches-switches
         self.addLink(h1, s1)       
         self.addLink(h2, s2)
         self.addLink(h3, s3)
         self.addLink(h4, s4)
+        self.addLink(h5, s5)
+        
         #self.addLink(nat1, s3)
         self.addLink(s1, s2)
         self.addLink(s2, s3)
         self.addLink(s1, s4)
         self.addLink(s4, s3)
+        self.addLink(s3, s5)
+        self.addLink(s4, s5)
 
 #dicionario da topologia, utilizado como parametro no momento da execucao no ambinente mininet
 topos = { 'linear': ( lambda: Linear() ) }
